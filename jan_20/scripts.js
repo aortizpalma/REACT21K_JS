094f1f70f87bb6076021533fe773b22265aaf677
Alfonso <aortizpalma@gmail.com>
2021-01-21 20:43:05 +0200

    Exercises from January 20-21

---
+++ jan_20/scripts.js
@@ -0,0 +1,39 @@
+/* let chosenNumber = Match.floor(Math.random() * 10);
+
+switch (chosenNumber) {
+    case 1:
+        console.log('One');
+        break;
+    case 2:
+        console.log('Two');
+        break;
+    case 3:
+        console.log('Three');
+        break;
+    case 4:
+        console.log('Four');
+        break;
+    case 5:
+        console.log('Five');
+        break;
+    case 6:
+        console.log('Six');
+        break;
+    default:
+        console.log("I don't know");
+        break;
+} */
+
+
+
+
+/* console.log('some text here');
+
+const message = 'Hello world from constant variable message';
+
+console.log(message);
+
+let emptyMessage = 'New message in let variable';
+
+console.log(emtyMessage);
+ */
